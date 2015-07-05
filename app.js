c008ed0326f0d4213718d620179e1ee25a525978
candido morante <candidomorantepelaez@hotmail.com>
2015-07-06 00:25:12 +0200

    Titulo Cambiado y estructura express

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('Uppps creo que grindeando me carge esta pagina!!!');
     err.status = 404;
     next(err);
 });
