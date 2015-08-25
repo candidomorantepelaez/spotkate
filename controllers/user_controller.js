40e08abb3ba4bfde8e334e5236746f6c3d951835
candido morante <candidomorantepelaez@hotmail.com>
2015-08-25 20:40:32 +0200

    creado el registro, edicion y borrado de usuarios

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,17 @@
 var models = require('../models/models.js');
 
+//Autoload :userId
+exports.load = function(req, res, next, userId){
+	models.User.find({where:{id: Number(userId)}}).then(function(user){
+		if(user){
+			req.user = user;
+			next();
+		}else{
+			next(new Error('No existe el usuario'))
+		}
+	}).catch(function(error){next(error)});
+};
+
 //Comprueba si el usuario esta registrado en users
 //si autenticacion falla o hay errores se ejecuta callback(error).
 exports.autenticar = function(login, password, callback){	
@@ -16,4 +28,61 @@ exports.autenticar = function(login, password, callback){
 				callback(new Error('No existe el usuario= '+login));
 			}
 		}).catch(function(error){callback(error)});
+};
+
+//GET/user/:id/edit
+exports.edit = function(req, res){
+	res.render('user/edit', {user:req.user, errors:[]});
+	//req.user: instancia de user cargada por autoload
+};
+
+//GET/user
+exports.new = function(req, res){
+	var user = models.User.build({username:"", password:""});//crea el objeto user
+	res.render('user/new', {user:user, errors:[]});
+};
+
+//POST/user
+exports.create = function(req, res, next){
+	var user = models.User.build(req.body.user);
+	
+	user.validate().then(function(err){
+		if(err){
+			res.render('user/new', {user:user, errors:err.errors});
+		}else{
+			user.save({fields:["username", "password"]}).then(function(){
+				req.session.user = {id:user.id, username:user.username};
+				res.redirect('/');
+			});
+		}
+	}).catch(function(error){next(error)});
+};
+
+//PUT/user/:id
+exports.update = function(req, res, next){
+	req.user.username = req.body.user.username;
+	req.user.password = req.body.user.password;
+	
+	req.user.validate().then(function(err){
+		if(err){
+			res.render('user/'+req.user.id, {user: req.user, errors:err.errors});
+		}else{
+			req.user.save({fields:["username", "password"]}).then(function(){
+				res.redirect('/');//redireccion http a /
+			});
+		}
+	}).catch(function(error){next(error)});
+};
+
+//DELETE/user/:id
+exports.destroy = function(req, res, next){
+	req.user.destroy().then(function(){
+		//borra la sesion y redirige a /
+		delete req.session.user;
+		res.redirect('/');
+	}).catch(function(error){next(error)});
+};
+//GET/user/:userId
+exports.show = function(req, res){
+	res.render('user/show', {user:req.user, errors:[]});
 };
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ var ShopController = require('../controllers/shop_controller');
 var CommentSpot = require('../controllers/commentSpot_controller');
 var CommentShop = require('../controllers/commentShop_controller');
 var SessionController = require('../controllers/session_controller');
+var UserController = require('../controllers/user_controller');
 
 /* GET home page. */
 router.get('/', Controller.index);
@@ -17,26 +18,35 @@ router.get('/login', SessionController.new); //formulario de login
 router.post('/login', SessionController.create); //crear sesion
 router.get('/logout', SessionController.destroy); //destruir sesion
 
+//Rutas de Cuentas
+router.param('userId', UserController.load);//autoload de cuenta
+router.get('/user', UserController.new);//formulario de registro
+router.post('/user', UserController.create);//creacion de usuario
+router.get('/user/:userId/edit', SessionController.loginRequired, UserController.edit);//formulario de edicion de usuario
+router.put('/user/:userId', SessionController.loginRequired, UserController.update);//editar el usuario
+router.get('/user/:userId', UserController.show);//muestra los datos de un user
+router.delete('/user/:userId', SessionController.loginRequired, UserController.destroy);//eliminar usuario
+
 //Rutas de Maps
-router.get('/maps', MapsController.maps);
+router.get('/maps', MapsController.maps);//muestra el mapa
 
 //Rutas de Spots
 router.param('spotsId', SpotController.load);//autoload de spots
-router.get('/spots', SpotController.spots);
-router.get('/spot/new', SessionController.loginRequired, SpotController.new);
-router.post('/spot/create', SessionController.loginRequired, SpotController.create);
-router.get('/spot/:spotsId/edit', SessionController.loginRequired, SpotController.edit);
-router.put('/spot/:spotsId', SessionController.loginRequired, SpotController.update);
-router.get('/spot/:spotsId', SpotController.show);
-router.delete('/spot/:spotsId', SessionController.loginRequired, SpotController.destroy);
+router.get('/spots', SpotController.spots);//muestra los spots
+router.get('/spot/new', SessionController.loginRequired, SpotController.new);//formulario de creacion de spot
+router.post('/spot/create', SessionController.loginRequired, SpotController.create);//creacion de spot
+router.get('/spot/:spotsId/edit', SessionController.loginRequired, SpotController.edit);//formulario de edicion de spot
+router.put('/spot/:spotsId', SessionController.loginRequired, SpotController.update);//editar el spot
+router.get('/spot/:spotsId', SpotController.show);//muestra los datos de un spot
+router.delete('/spot/:spotsId', SessionController.loginRequired, SpotController.destroy);//elimina un spot
 
 //Rutas de CommentSpot
-router.get('/spot/:spotsId/comments/new',SessionController.loginRequired, SessionController.loginRequired, CommentSpot.new);
-router.post('/spot/:spotsId/comments',SessionController.loginRequired, SessionController.loginRequired, CommentSpot.create);
+router.get('/spot/:spotsId/comments/new',SessionController.loginRequired, SessionController.loginRequired, CommentSpot.new);//formulario de creacion de comentario de spot
+router.post('/spot/:spotsId/comments',SessionController.loginRequired, SessionController.loginRequired, CommentSpot.create);//creacion de comentario de spot
 
 //Rutas de Shops
 router.param('shopsId', ShopController.load);//autoload de shops
-router.get('/shops', ShopController.shops);
+router.get('/shops', ShopController.shops);//muestra las ahops
 router.get('/shop/new', SessionController.loginRequired, ShopController.new);
 router.post('/shop/create', SessionController.loginRequired, ShopController.create);
 router.get('/shop/:shopsId/edit', SessionController.loginRequired, ShopController.edit);
